d93fc939833e9b00379d1016c2d246a1b1ff99e5
bogo410 <alberto.bogoni@hotmail.it>
2015-12-24 10:46:20 +0100

    aggiunto ALTER TABLE + ADD, RENAME e DROP COLUMN
    DROP TABLE non funziona ancora

--- old/4/prova.sql
+++ old/4/prova.sql
@@ -1,21 +1,16 @@
-//prova.sql
-
 CREATE DATABASE Spy; 
 CREATE TABLE Student();
-CREATE TABLE Students(Id SMALLINT, Name VARCHAR, Surname VARCHAR);
-ALTER TABLE Student ADD Anno SMALLINT;
-ALTER TABLE Students ADD Anno SMALLINT;
-
+CREATE TABLE Students(Id SMALLINT, Name VARCHAR, Surname VARCHAR, Prova VARCHAR);
 CREATE TABLE Newtable1();
 CREATE TABLE Newtable2();
 CREATE TABLE Newtable3();
+ALTER TABLE Student ADD Anno SMALLINT;
+ALTER TABLE Students ADD Anno SMALLINT;
 ALTER TABLE Students DROP COLUMN Surname;
 ALTER TABLE Student DROP COLUMN Anno;
 ALTER TABLE Student ADD AnnnoAccademico SMALLINT;
-ALTER TABLE Students DROP Name;
 ALTER TABLE Student RENAME COLUMN AnnnoAccademico TO NewAnnnoAccademico;
-
-ALTER TABLE Student RENAME TO NewStudent;
+ALTER TABLE Students DROP Prova;
 DROP TABLE NewTable1;
 DROP TABLE NewTable2, Newtable3;
-
+ALTER TABLE Student RENAME TO NewStudent;
