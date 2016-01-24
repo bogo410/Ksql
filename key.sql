dc6a71e6316c4ef256fe94e2038bdd86a1efbf57
bogo410 <alberto.bogoni@hotmail.it>
2016-01-24 14:38:09 +0100

    aggiunto controllo sulle primary key funzionante

--- key.sql
+++ key.sql
@@ -2,13 +2,13 @@
 
 CREATE DATABASE Spy;
 
-CREATE TABLE Chiave (key SMALLINT PRIMARY KEY, notkey VARCHAR(4) );
+CREATE TABLE Chiave (key SMALLINT , notkey VARCHAR(10) PRIMARY KEY);
 
 INSERT INTO Chiave (key,notkey) VALUES (7,'"ciao"');
-INSERT INTO Chiave (notkey,key) VALUES ('"ciao"',14);
+INSERT INTO Chiave (notkey,key) VALUES ('"aiao"',14);
 
-INSERT INTO Chiave (key) VALUES (10);
+//INSERT INTO Chiave (key) VALUES (10);
 
-INSERT INTO Chiave (key) VALUES (10);
+//INSERT INTO Chiave (key) VALUES (78);
 
-//INSERT INTO Chiave (notkey) VALUES ('"ciao"');
+INSERT INTO Chiave (notkey) VALUES ('"ciaone"');
