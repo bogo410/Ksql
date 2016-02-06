57e6d69a9cb13dec6696d87b6f55e367506435bb
bogo410 <alberto.bogoni@hotmail.it>
2016-02-06 10:53:07 +0100

    update base funzionante solo per il primo valore

--- update.sql
+++ update.sql
@@ -6,5 +6,5 @@ CREATE TABLE Prova (Try SMALLINT , Pry VARCHAR(10), Cry VARCHAR);
 INSERT INTO Prova (Try) VALUES (5);
 INSERT INTO Prova (Try,Pry) VALUES (7,'"ciao"');
 
-UPDATE Prova SET Try = 10;
+UPDATE Prova SET Try = '10';
 //UPDATE Prova SET Try = 10 , Pry = '"def"';
