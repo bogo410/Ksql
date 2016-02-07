62de48a1575d25c1534af149db813c84b9d60278
bogo410 <alberto.bogoni@hotmail.it>
2016-02-07 18:09:22 +0100

    aggiunto controllo tipi per l'update

--- update.sql
+++ update.sql
@@ -6,6 +6,11 @@ CREATE TABLE Prova (Try SMALLINT , Pry VARCHAR(10), Cry VARCHAR);
 INSERT INTO Prova (Try) VALUES (5);
 INSERT INTO Prova (Try,Pry) VALUES (7,'"ciao"');
 
-UPDATE Prova SET Try = '"ggg"';
-UPDATE Prova SET Pry = '10';
+UPDATE Prova SET Try = '10';
+UPDATE Prova SET Pry = '"miao"';
+UPDATE Prova SET Cry = '"biao"';
+
+UPDATE Prova SET Cry = '5';
+UPDATE Prova SET Pry = '"miao5678910"';
+
 //UPDATE Prova SET Try = 10 , Pry = '"def"';
