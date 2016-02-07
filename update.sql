550f35f5d35e0985c542c11725b07314eb00bbe1
bogo410 <alberto.bogoni@hotmail.it>
2016-02-07 18:38:17 +0100

    aggiunta la possibilità di avere più sostituzioni nel comando update semplice

--- update.sql
+++ update.sql
@@ -6,11 +6,13 @@ CREATE TABLE Prova (Try SMALLINT , Pry VARCHAR(10), Cry VARCHAR);
 INSERT INTO Prova (Try) VALUES (5);
 INSERT INTO Prova (Try,Pry) VALUES (7,'"ciao"');
 
+UPDATE Prova SET Try = '20' , Pry = '"def"';
+
 UPDATE Prova SET Try = '10';
 UPDATE Prova SET Pry = '"miao"';
 UPDATE Prova SET Cry = '"biao"';
 
-UPDATE Prova SET Cry = '5';
-UPDATE Prova SET Pry = '"miao5678910"';
+//UPDATE Prova SET Cry = '5';
+//UPDATE Prova SET Pry = '"miao5678910"';
 
-//UPDATE Prova SET Try = 10 , Pry = '"def"';
+UPDATE Prova SET Try = '20' , Pry = '"def"';
